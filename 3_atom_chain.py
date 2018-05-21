8e88c3f4822d3bdf325103e5901c030be5938aaf
Fagin Hales <fhales@lcn.ucl.ac.uk>
2018-05-21 22:28:50 +0100

    added gate optimisation

--- 3_atom_chain.py
+++ 3_atom_chain.py
@@ -7,13 +7,17 @@ Created on Fri May 18 16:57:21 2018
 from qutip import *
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 I = qeye(2)
-times = np.linspace(0.0, 100.0, 1000.0)
-C3=1
+times = np.linspace(0.0, 1.0, 10.0)
+C3 = 1
 
 
-qubits = [[0,0],[1,0],[2,0]]
+initqubits = [0,0,0,1,0,2,0,3]
+
+outputs = [tensor(fock(2,0),fock(2,0))*tensor(fock(2,0),fock(2,0)).dag(),tensor(fock(2,0),fock(2,1))*tensor(fock(2,0),fock(2,1)).dag(),tensor(fock(2,1),fock(2,1))*tensor(fock(2,1),fock(2,1)).dag(),tensor(fock(2,1),fock(2,0))*tensor(fock(2,1),fock(2,0)).dag()]
+
 
 def makematrix(qubits_co):
     intmatrix = []
@@ -24,12 +28,6 @@ def makematrix(qubits_co):
         intmatrix.append(temprow)
     return intmatrix
         
-def makeinputoutput(atom_number):
-    tempstatein = fock(2,1)
-    for i in range(atom_number-1):
-        tempstatein = tensor(tempstatein,fock(2,0))
-    tempstateout = tempstatein*tempstatein.dag()
-    return tempstatein, tempstateout
 
 def makesig(i,j,atoms):
     temp1 = [I]*atoms
@@ -57,8 +55,43 @@ def makeplot(H,timesteps,instate,basis):
     data = mcsolve(H,instate,timesteps,[],basis)
     plt.plot(timesteps, data.expect[0])
 
-def doall(qubits,times):
+def getdata(qubits,times,input_state):
+    input_state = tensor(input_state,fock(2,0),fock(2,1))
     int_matrix = makematrix(qubits)
-    input_state, output_basis = makeinputoutput(len(qubits))
     H = makeham(int_matrix)
-    makeplot(H,times,input_state,output_basis)
+    data = mcsolve(H,input_state,times,[],[]).states[-1].ptrace([0,1])
+    return data
+
+def makeallstates(n):
+    states = []
+    for i in range(n):
+        binn = bin(i)[2:]
+        while len(binn) != len(bin(n)[2:])-1:
+            binn = '0' + binn
+        tempstate = [fock(2,int(binn[j])) for j in range(len(binn))]
+        states.append(tensor(tempstate))
+    return states
+
+def calfil(qubits):
+    qubits = np.array(qubits)
+    lent = int(len(qubits)/2)
+    qubits.resize((lent,2))
+    qubits = qubits.tolist()
+    n = len(outputs)
+    states = makeallstates(n)
+    data = []
+    for i in range(n):
+        data.append(getdata(qubits,times,states[i]))
+    fidels = [fidelity(data[k],outputs[k]) for k in range(n)]
+    fidel = np.average(fidels)
+    return -1*fidel
+
+def solve(initqubits):
+    res = minimize(calfil, initqubits, method='nelder-mead',options={'xtol': 1e-8, 'disp': True})
+    return res
+
+
+
+
+
+
